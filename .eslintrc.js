ab5869be047eabf1c8200afe77b0ad88dfd789b0
spdev <spdevuk@gmail.com>
2018-04-03 22:35:07 +0100

    added react and proxy

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,4 @@
 module.exports = {
   extends: 'standard',
-  parserOptions: {
-    ecmaVersion: 6
-  },
   plugins: ['jsx-a11y']
 };
---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <a href="/auth/google">Sign in with google</a>
+      </div>
+    );
+  }
+}
+
+export default App;
