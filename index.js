31e3099177d1c7fa244588357ada33191eb09ee7
spdev <spdevuk@gmail.com>
2018-04-03 14:59:12 +0100

    Added user schema

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+require('./models/User');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
 require('./routes/authroutes')(app);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+
+app.listen(PORT, function() {
+  console.log('Server starting..');
+});
