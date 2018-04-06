ed6b7c3b75d105c69b82ea3f62f9ddd9f2b1f401
spdev <spdevuk@gmail.com>
2018-04-06 02:46:52 +0100

    added survey middlewares&routes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const keys = require('./config/keys');
 const bodyParser = require('body-parser');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
@@ -23,6 +24,7 @@ app.use(passport.session());
 
 require('./routes/authroutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
